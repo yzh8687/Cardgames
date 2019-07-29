c3ba942b42a7f63afffbde370914c3d4c3a48428
Zhaoheng Yang <yzh8687@gmail.com>
2019-07-29 13:45:09 -0400

    Added javadoc for Card.java.

--- src/Deck/Card.java
+++ src/Deck/Card.java
@@ -4,23 +4,46 @@ import java.util.ArrayList;
 
 public class Card {
     private int number;
-    private int colour;
+    private int color;
 
-    public Card(int number, int colour) {
+    /**
+     * Initialize a card object.
+     *
+     * @param number The number of this card.
+     * @param color The index of the color of this card
+     */
+    public Card(int number, int color) {
         this.number = number;
-        this.colour = colour;
+        this.color = color;
     }
 
+    /**
+     * Return the number of this card.
+     *
+     * @return The number of this card.
+     */
     public int getNumber() {
         return number;
     }
 
-    public int getColour() {
-        return colour;
+    /**
+     * Return the index of the color of this card.
+     *
+     * @return The index of the color of this card.
+     */
+    public int getColor() {
+        return color;
     }
 
-    private String getColourName() {
-        switch (colour) {
+    /**
+     * Return the specific color for each index.
+     *
+     * '0' for Spades, '1' for Hearts, '2' for Clubs, '3' for Diamonds and '4' for Jokers.
+     *
+     * @return The specific color for each index.
+     */
+    private String getColorName() {
+        switch (color) {
             case 0:
                 return "♠";
             case 1:
@@ -36,6 +59,16 @@ public class Card {
         }
     }
 
+    /**
+     * Return the specific card name according to the card number.
+     *
+     * Return 'A' for '1' and '14' ('A' is '14' when in this deck 'A' is larger than 'K').
+     * Return '2' for '2' and '15' ('2' is '15' when in this deck '2' is larger than 'A' where 'A' is '14').
+     * Return 'J', 'Q', 'K' for '11', '12', '13' correspondingly.
+     * Return '●' for '16' which is black joker and '○' for '17' which is red joker.
+     *
+     * @return The specific card name according to the card number.
+     */
     private String getNumberName() {
         if (number == 1 || number == 14) {
             return "A";
@@ -56,15 +89,33 @@ public class Card {
         }
     }
 
+    /**
+     * Set the number of this card to the given <number>.
+     *
+     * @param number The new card number which is going to set to this card.
+     */
     public void setNumber(int number) {
         this.number = number;
     }
 
+    /**
+     * Return the visible information of this card.
+     *
+     * @return The visible information of this card.
+     */
     @Override
     public String toString() {
-        return "{" + getColourName() + getNumberName() + "}";
+        return "{" + getColorName() + getNumberName() + "}";
     }
 
+    /**
+     * Check whether the cards from index <start> to <end> of the given <suit> all have the same number.
+     *
+     * @param suit An ArrayList of cards.
+     * @param start The start index for checking.
+     * @param end The end index for checking.
+     * @return Return true if the cards in the given interval have the same number, false if not.
+     */
     public static boolean equalCards(ArrayList<Card> suit, int start, int end) {
         for (int i = start; i < end; i ++) {
             if (suit.get(i).getNumber() != suit.get(i + 1).getNumber()) {
@@ -74,6 +125,12 @@ public class Card {
         return true;
     }
 
+    /**
+     * Check whether all the cards in the given <suit> have the same number.
+     *
+     * @param suit An ArrayList of cards.
+     * @return Return true if all the cards in the given <suit> have the same number, false if not.
+     */
     public static boolean equalCards(ArrayList<Card> suit) {
         return equalCards(suit, 0, suit.size() - 1);
     }
